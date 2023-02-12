09a25e5153e4c99fd03eb6e8a973b2af41ecb4e0
Your Name <you@example.com>
2023-02-12 15:45:29 +0100

    #

--- databise.py
+++ databise.py
@@ -1,15 +1,32 @@
-# import sqlite module :
+# import sqlite module:
 
 import sqlite3
 
-# create Database and conect
+# create data base :
+db = sqlite3.connect("rajae.db")
 
-db = sqlite3.connect("amin.db")
+# setting curser :
 
-# create tabel :
-db.execute(
-    "CREATE TABLE IF NOT EXISTS rajae (name text, progress integer, user_id integer)")
+cr = db.cursor()
 
-#  close database :
+# create table and Fields :
 
-db.close()
+cr.execute(
+    "CREATE TABLE IF NOT EXISTS Myskills (name text, progress integer, user_id integer)")
+cr.execute(
+    "CREATE TABLE IF NOT EXISTS User_Client (user_id integer, name text)")
+
+cr.execute("CLEAR TABLE User_Client")
+myNAme = ["amin", "hamid", "khalid"]
+
+for k, name in enumerate(myNAme, 1):
+    cr.execute(f"INSERT INTO User_Client(user_id, name) VALUES({k},'{name}')")
+
+# cr.execute("INSERT INTO User_Client(user_id, name) VALUES(1,'Rajae')")
+# cr.execute("INSERT INTO User_Client(user_id, name) VALUES(2,'Amin')")
+# cr.execute("INSERT INTO User_Client(user_id, name) VALUES(3,'Zineb')")
+# cr.execute("INSERT INTO User_Client(user_id) VALUES(3)")
+
+db.commit()
+
+cr.close()
